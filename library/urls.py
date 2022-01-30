85c944c544a5a6cbc32cca9f7037c88fca6ae633
WendSant <wendsonsantanajps@gmail.com>
2022-01-30 02:02:16 -0300

    initial commit

---
+++ library/urls.py
@@ -0,0 +1,20 @@
+
+
+from posixpath import basename
+from django.contrib import admin
+from django.urls import path, include
+from books.api.viewsets import BooksViewSet
+
+from rest_framework import routers
+from books.api import viewsets as booksviewsets
+
+
+route = routers.DefaultRouter()
+
+route.register(r'books', booksviewsets.BooksViewSet, basename='Books')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(route.urls))
+]
